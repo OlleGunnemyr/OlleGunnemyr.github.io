5245bd08c150913ffb321c86ced2afd73df8bd1a
OlleGunnemyr <ollegu@kth.se>
2021-11-25 15:16:38 +0100

    BLALA

--- Operativsystem/Övning2/HelloDolly.c
+++ Operativsystem/Övning2/HelloDolly.c
@@ -3,13 +3,22 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 int main() {
-int pid = fork();
-if (pid == 0) {
-printf("I'm the child %d\n" , getpid());
-sleep(1);
+int pid ;
+int x = 123;
+pid = fork() ;
+if ( pid == 0 ) {
+printf( "child: x is %d\n" , x ) ;
+x = 42;
+sleep(1) ;
+printf( "child: x is %d\n" , x ) ;
 } else {
-printf("My child is called %d\n" , pid);
+printf( "mother: x is %d\n" , x ) ;
+x = 13;
+sleep(1) ;
+printf( "mother : x is %d\n" , x ) ;
+wait (NULL) ;
 }
-printf("That's it %d\n" , getpid());
 return 0 ;
 }
+
+
